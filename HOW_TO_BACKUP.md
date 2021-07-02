babb99735abd5505821c0a74a9d758cfdbc13002
Justin Mathews <github@justinmathews.ca>
2021-07-02 00:49:28 -0400

    send backups to the specified b2 bucket

---
+++ HOW_TO_BACKUP.md
@@ -0,0 +1,13 @@
+# How To Prep For Backups
+
+- install `rclone`
+- `rclone config`
+    - "New remote"
+    - name: "b2"
+    - key ID and Application Key as stored in password manager
+- create a bucket in B2 web interface
+
+
+
+  
+TODO: rclone config, Set configuration password
--- backup.py
+++ backup.py
@@ -8,7 +8,10 @@ import json
 import logging
 import os
 import signal
+import socket
+import subprocess
 import sys
+from datetime import datetime
 
 verbosity_to_log_level = {
     0: logging.CRITICAL,
@@ -22,22 +25,47 @@ verbosity_to_log_level = {
 class Tracker():
     _interrupt_requested = False
 
-    def __init__(self, filename, sources) -> None:
+    def __init__(self, filename, sources, remote_name, bucket_name) -> None:
         self._filename = filename
         self._top_level_sources = sources
+        self._remote_name = remote_name
+        self._bucket_name = bucket_name
         self._detailed_sources = []
         self._tracker = {}
 
         signal.signal(signal.SIGINT, Tracker._sigint_handler)
-        if os.path.isfile(self._filename):
-            logging.debug(f"{self._filename} exists, we'll use that for backups")
+        if os.path.isfile(self.filename):
+            logging.debug(f"{self.filename} exists, we'll use that for backups")
             self._load_from_disk()
         else:
-            logging.debug(f"{self._filename} doesn't exist, generating from {self._top_level_sources}")
+            logging.debug(f"{self.filename} doesn't exist, generating from {self.top_level_sources}")
             self.make_fresh_tracker()
             self._save_to_disk()
         if self._interrupt_requested:
             sys.exit(1)
+
+    @property
+    def filename(self):
+        return self._filename
+
+    @property
+    def top_level_sources(self):
+        return self._top_level_sources
+
+    @property
+    def remote_name(self):
+        return self._remote_name
+
+    @property
+    def bucket_name(self):
+        return self._bucket_name
+
+    @property
+    def hostname(self):
+        return socket.gethostname()
+    @property
+    def dest_prefix(self):
+        return f"{self.remote_name}:{self.bucket_name}/{self.hostname}"
 
     @staticmethod
     def _sigint_handler(sig, frame):
@@ -58,30 +86,40 @@ class Tracker():
             "sources": tracker_sources
         }
 
-    @property
-    def filename(self):
-        return self._filename
-
-    @property
-    def top_level_sources(self):
-        return self._top_level_sources
-
     def _load_from_disk(self):
-        with open(self._filename, "r") as tracker_file:
+        with open(self.filename, "r") as tracker_file:
             self._tracker = json.load(tracker_file)
 
     def _save_to_disk(self):
-        with open(self._filename, "w") as tracker_file:
+        with open(self.filename, "w") as tracker_file:
             json.dump(self._tracker, tracker_file, indent=2)
 
     def resume_backups(self):
         next_source = self._tracker["next"]
         sources = self._tracker["sources"]
         while not self._interrupt_requested and (source := sources.get(str(next_source))):
-            logging.info("rclone " + source["path"])
-            next_source += 1
-            self._tracker["next"] = next_source
-            self._save_to_disk()
+            backup_command = [
+                'rclone',
+                'copy',
+                source["path"],
+                f'{self.dest_prefix}{source["path"]}',
+            ]
+            logging.info(" ".join(backup_command))
+            try:
+                subprocess.run(backup_command, capture_output=True, check=True, encoding="ascii")
+                next_source += 1
+                self._tracker["next"] = next_source
+                source["done"] = datetime.utcnow().isoformat()
+                self._save_to_disk()
+            except subprocess.CalledProcessError as exception:
+                error_message = f"\n" \
+                                f"{exception.returncode=}\n" \
+                                f"{exception.cmd=}\n" \
+                                f"{exception.output=}\n" \
+                                f"{exception.stdout=}\n" \
+                                f"{exception.stderr=}\n"
+                logging.error(error_message)
+                break
         logging.info("done")
 
 
@@ -90,14 +128,24 @@ def main():
     parser.add_argument("-t", "--tracker",
                         help="progress tracking file to support resuming after interruption",
                         type=str,
-                        nargs=1,
                         default="backup_tracker.json",
+                        )
+    parser.add_argument("-r", "--remote-name",
+                        help="The name given to the B2 destination remote in your rclone config",
+                        type=str,
+                        required=True,
+                        )
+    parser.add_argument("-b", "--bucket-name",
+                        help="The name given to the B2 bucket where backups will be stored",
+                        type=str,
+                        required=True,
                         )
     parser.add_argument("-s", "--sources",
                         help="absolute path[s] to be backed up",
                         type=str,
                         nargs="+",
                         default=[os.getcwd()],
+                        required=True,
                         )
     parser.add_argument("-v", "--verbose",
                         help="verbosity of logging, -v is minimal, -vvvv is verbose",
@@ -108,7 +156,7 @@ def main():
     logging.basicConfig(level=verbosity_to_log_level.get(args.verbose, 2), format='%(asctime)s: %(levelname)s:\t%(message)s')
 
     tracker_filename = args.tracker
-    tracker = Tracker(tracker_filename, args.sources)
+    tracker = Tracker(tracker_filename, args.sources, args.remote_name, args.bucket_name)
     tracker.resume_backups()
 
 
