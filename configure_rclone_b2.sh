4fd289f8e2b2df4594381e33d789b8cfaa52156e
Justin Mathews <github@justinmathews.ca>
2021-07-03 21:00:11 -0400

    check for undefined env vars

--- configure_rclone_b2.sh
+++ configure_rclone_b2.sh
@@ -4,6 +4,30 @@ if [[ ! `command -v rclone` ]]; then
 	curl https://rclone.org/install.sh | sudo bash
 fi
 
-rclone config create b2 b2 account ${ACCOUNTID} key ${KEYID}
-rclone config create b2crypt crypt remote ${CRYPT_REMOTE} filename_encryption "off" directory_name_encryption "false" password ${PASSWORD} password2 ${SALT}
+if [[ -z ${ACCOUNTID} ]]; then
+    echo "\$ACCOUNTID is not defined. Please set this environment variable. Check your password manager for the correct value."
+    exit 1
+fi
 
+if [[ -z ${KEYID} ]]; then
+    echo "\KEYID is not defined. Please set this environment variable. Check your password manager for the correct value."
+    exit 1
+fi
+
+if [[ -z ${CRYPT_REMOTE} ]]; then
+    echo "\CRYPT_REMOTE is not defined. Please set this environment variable. Check your password manager for the correct value."
+    exit 1
+fi
+
+if [[ -z ${PASSWORD} ]]; then
+    echo "\PASSWORD is not defined. Please set this environment variable. Check your password manager for the correct value."
+    exit 1
+fi
+
+if [[ -z ${SALT} ]]; then
+    echo "\SALT is not defined. Please set this environment variable. Check your password manager for the correct value."
+    exit 1
+fi
+
+rclone config create b2 b2 account "${ACCOUNTID}" key "${KEYID}"
+rclone config create b2crypt crypt remote "${CRYPT_REMOTE}" filename_encryption "off" directory_name_encryption "false" password "${PASSWORD}" password2 "${SALT}"
