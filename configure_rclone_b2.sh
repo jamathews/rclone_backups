b61deb4ce311dcaa73bb8072a9477419ada2c2bd
github@justinmathews.ca <github@justinmathews.ca>
2021-07-03 20:55:55 -0400

    configure remotes for backup

--- configure_rclone_b2.sh
+++ configure_rclone_b2.sh
@@ -1,2 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ ! `command -v rclone` ]]; then
+	curl https://rclone.org/install.sh | sudo bash
+fi
+
+rclone config create b2 b2 account ${ACCOUNTID} key ${KEYID}
+rclone config create b2crypt crypt remote ${CRYPT_REMOTE} filename_encryption "off" directory_name_encryption "false" password ${PASSWORD} password2 ${SALT}
+
